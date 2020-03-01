5ff2f8727415262e206fea5790804748cb374943
felixatchur <felix@churinc.com>
2020-03-01 22:25:17 +1100

    migrate geocode and forecast function; accessing api from browser

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -39,9 +41,47 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an adderss'
+    })
+  }
+
+  geocode(req.query.address, (error, {lat, lng, location} = {}) => {
+    if (error) {
+      return res.send({ error })
+    }
+
+    forecast(lat, lng, (error, forecastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      })
+    })
+  })
+
+
+  // res.send({
+  //   location: req.query.address,
+  //   forecast: 'Sunny'
+  // })
+})
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+
+  console.log(req.query.search)
   res.send({
-    location: 'Sydney',
-    forecast: 'Sunny'
+    products: []
   })
 })
 
